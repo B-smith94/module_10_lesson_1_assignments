b2411568b32095437fd3d5e5ec983888258b774e
b-smith94 <byrondsmith9@gmail.com>
2024-11-14 15:10:37 -0500

    outlined each task using comments

--- assignment1.js
+++ assignment1.js
@@ -7,17 +7,20 @@ Task 2: Assign sample values to ethe student information variables
 Task 3: Display the student information using console.log() statements
 */
 
+//Task 1
 let studentName;
 let studentAge;
 let studentGrade;
 let isPresent;
 
+//Task 2
 studentName = "Alice";
 studentAge = 15;
 studentGrade = 10;
 isPresent = true;
 
-console.log("Student Name:", studentName)
-console.log("Student Age:", studentAge)
-console.log("Student Grade:", studentGrade)
-console.log("Is the Student Present?", isPresent)
+//Task 3
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Student Grade:", studentGrade);
+console.log("Is the Student Present?", isPresent);
