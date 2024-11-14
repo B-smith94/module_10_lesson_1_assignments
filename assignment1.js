366b81b7c239a9ed85e3d5152ed1033715f5f696
b-smith94 <byrondsmith9@gmail.com>
2024-11-14 14:49:53 -0500

    Completed Assignment 1

---
+++ assignment1.js
@@ -0,0 +1,23 @@
+/*Assignment 1: Understanding Data Types in JavaScript
+
+Task 1: Declare variables to represent student informatino such as name, age, grade, and attendance status
+
+Task 2: Assign sample values to ethe student information variables
+
+Task 3: Display the student information using console.log() statements
+*/
+
+let studentName;
+let studentAge;
+let studentGrade;
+let isPresent;
+
+studentName = "Alice";
+studentAge = 15;
+studentGrade = 10;
+isPresent = true;
+
+console.log("Student Name:", studentName)
+console.log("Student Age:", studentAge)
+console.log("Student Grade:", studentGrade)
+console.log("Is the Student Present?", isPresent)
